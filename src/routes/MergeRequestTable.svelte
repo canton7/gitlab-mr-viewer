75cbf6db8266e5816b1e13388ef9e7adaae41cda
Antony Male <antony.male@gmail.com>
2025-08-17 09:04:55 +0100

    WIP looking at approvals and discussions

--- src/routes/MergeRequestTable.svelte
+++ src/routes/MergeRequestTable.svelte
@@ -4,34 +4,54 @@
 
     interface Props {
         mergeRequests: MergeRequest[];
-        showAuthor: boolean;
+        showAuthor?: boolean;
     }
 
     let { mergeRequests, showAuthor = false }: Props = $props();
 </script>
 
+{#if mergeRequests.length == 0}
+    <p class="caught-up">All caught up!</p>
+{/if}
+
 <div class="merge-request-table">
     {#each mergeRequests as mr (mr.id)}
         <div>
-            <p><a href={mr.web_url}>{mr.title}</a></p>
+            <p><a href={mr.webUrl}>{mr.title}</a></p>
             <p class="subtitle">
-                {mr.reference} · {moment(mr.created_at).fromNow()}
+                {mr.reference} · {moment(mr.createdAt).fromNow()}
                 {#if showAuthor}
-                    by {mr.author_name}
+                    by {mr.authorName}
                 {/if}
             </p>
         </div>
-        <div>
-            <p class="subtitle">Updated {moment(mr.updated_at).fromNow()}</p>
+        <div class="column-2">
+            <div>
+                {mr.resolvedDiscussions} / {mr.totalDiscussions}
+                {#if mr.isApproved}
+                    <i class="approved fa-regular fa-circle-check"></i>
+                {/if}
+            </div>
+            <p class="subtitle">Updated {moment(mr.updatedAt).fromNow()}</p>
         </div>
     {/each}
 </div>
 
 <style>
+    .caught-up {
+        text-align: center;
+    }
+
     .merge-request-table {
         display: grid;
         grid-template-columns: 1fr auto;
+        grid-column-gap: 20px;
         grid-row-gap: 20px;
+    }
+
+    .merge-request-table > div {
+        display: flex;
+        flex-direction: column;
     }
 
     .merge-request-table a {
@@ -47,7 +67,18 @@
         margin: 0;
     }
 
+    .column-2 {
+        text-align: right;
+    }
+
     p.subtitle {
         font-size: 0.6rem;
+        margin-top: auto;
+    }
+
+    .approved {
+        color: green;
+        width: 100%;
+        text-align: right;
     }
 </style>
