fc507cdd1ab086191a50f98858f0a07ad8f95673
Antony Male <antony.male@gmail.com>
2025-08-21 22:01:55 +0100

    Move configuration to a settings screen

---
+++ src/lib/Settings.svelte.ts
@@ -0,0 +1,26 @@
+import { browser } from '$app/environment';
+import { writable } from 'svelte/store';
+
+function create<T>(name: string, defaultValue: T) {
+    const state = writable<T>(defaultValue);
+
+    if (browser) {
+        const value = window.localStorage.getItem(name);
+        if (value !== null) {
+            state.set(JSON.parse(value) as T);
+        }
+
+        state.subscribe((value) => {
+            window.localStorage.setItem(name, JSON.stringify(value));
+        });
+    }
+
+    return state;
+}
+
+interface GitlabSettings {
+    baseUrl?: string;
+    accessToken?: string;
+}
+
+export const gitlabSettings = create<GitlabSettings>('gitlabSettings', {});
