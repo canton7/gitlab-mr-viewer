31e8961ddd9ef997a2ce9efa37dd0fcf0d632183
Antony Male <antony.male@gmail.com>
2025-08-26 21:43:58 +0100

    More playing with bootstrap and bubbles

--- src/routes/MergeRequestTable.svelte
+++ src/routes/MergeRequestTable.svelte
@@ -19,20 +19,23 @@
         <div class="card p-2">
             <div class="headline">
                 <p><a href={mr.webUrl}>{mr.title}</a></p>
-                <div>
+                <div class="bubbles">
                     {#if mr.totalDiscussions > 0}
                         <div class="bubble">
-                            <i class="fa-regular fa-comment"></i>
                             {mr.resolvedDiscussions} / {mr.totalDiscussions}
+                            <i class="fa-solid fa-list-check"></i>
                         </div>
-                    {/if}
-                    {#if mr.isApproved}
-                        <i class="approved fa-regular fa-circle-check"></i>
                     {/if}
                     {#if mr.ciLink}
                         <a href={mr.ciLink}>{mr.ciStatus}</a>
                     {:else}
                         {mr.ciStatus}
+                    {/if}
+                    {#if mr.isApproved}
+                        <div class="bubble">
+                            Approved
+                            <i class="approved fa-regular fa-circle-check"></i>
+                        </div>
                     {/if}
                 </div>
             </div>
@@ -56,15 +59,29 @@
 
     .headline {
         display: flex;
+        /* Not ideal: bubbles take up more space than they need to */
         justify-content: space-between;
+        align-items: stretch;
+
+        .bubbles {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: flex-end;
+        }
     }
 
     .bubble {
-        display: inline-block;
+        display: inline-flex;
+        align-items: center;
+        font-size: 0.9rem;
         border: 1px solid black;
-        padding: 0 5px;
-        height: 25px;
-        border-radius: 25px;
+        padding: 2px 6px;
+        height: 1.5rem;
+        border-radius: 0.5rem;
+
+        i {
+            margin-left: 5px;
+        }
     }
 
     .footer {
