a50ebe33a6132f29bf75af1345646a9df3522678
Antony Male <antony.male@gmail.com>
2025-08-27 08:23:03 +0100

    Playing with badges, I think

--- src/routes/MergeRequestTable.svelte
+++ src/routes/MergeRequestTable.svelte
@@ -16,33 +16,40 @@
 
 <div class="merge-request-table">
     {#each mergeRequests as mr (mr.id)}
-        <div>
-            <p><a href={mr.webUrl}>{mr.title}</a></p>
-            <p class="subtitle">
-                {mr.reference} · {moment(mr.createdAt).fromNow()}
-                {#if showAuthor}
-                    by {mr.authorName}
-                {/if}
-            </p>
-        </div>
-        <div class="column-2">
+        <article class="border">
             <div>
-                <p>
-                    {#if mr.totalDiscussions > 0}
-                        {mr.resolvedDiscussions} / {mr.totalDiscussions}
-                    {/if}
-                    {#if mr.isApproved}
-                        <i class="approved fa-regular fa-circle-check"></i>
-                    {/if}
-                    {#if mr.ciLink}
-                        <a href={mr.ciLink}>{mr.ciStatus}</a>
-                    {:else}
-                        {mr.ciStatus}
+                <p><a href={mr.webUrl}>{mr.title}</a></p>
+                <p class="subtitle">
+                    {mr.reference} · {moment(mr.createdAt).fromNow()}
+                    {#if showAuthor}
+                        by {mr.authorName}
                     {/if}
                 </p>
             </div>
-            <p class="subtitle">Updated {moment(mr.updatedAt).fromNow()}</p>
-        </div>
+            <div class="column-2">
+                <div>
+                    <p>
+                        {#if mr.totalDiscussions > 0}
+                            {mr.resolvedDiscussions} / {mr.totalDiscussions}
+                        {/if}
+                        {#if mr.isApproved}
+                            <button class="small">
+                                <i>check</i>
+                                <span>Approved</span>
+                            </button>
+
+                            <!-- <i class="approved fa-regular fa-circle-check"></i> -->
+                        {/if}
+                        {#if mr.ciLink}
+                            <a href={mr.ciLink}>{mr.ciStatus}</a>
+                        {:else}
+                            {mr.ciStatus}
+                        {/if}
+                    </p>
+                </div>
+                <p class="subtitle">Updated {moment(mr.updatedAt).fromNow()}</p>
+            </div>
+        </article>
     {/each}
 </div>
 
@@ -52,16 +59,34 @@
     }
 
     .merge-request-table {
+        article {
+            padding: 10px;
+            display: grid;
+            grid-template-columns: 1fr auto;
+
+            button {
+                gap: 0.25rem;
+                font-size: 0.8rem;
+            }
+
+            & > div {
+                display: flex;
+                flex-direction: column;
+            }
+        }
+    }
+
+    /* .merge-request-table {
         display: grid;
-        grid-template-columns: 1fr auto;
+        grid-template-columns: auto;
         grid-column-gap: 20px;
         grid-row-gap: 20px;
     }
 
-    .merge-request-table > div {
+    .merge-request-table > article {
         display: flex;
         flex-direction: column;
-    }
+    } */
 
     .merge-request-table a {
         text-decoration: none;
@@ -72,7 +97,6 @@
     }
 
     .merge-request-table p {
-        font-size: 0.75rem;
         margin: 0;
     }
 
