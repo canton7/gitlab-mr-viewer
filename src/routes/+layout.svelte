8f7db7cfcd0a6a801d6b0b44a32d4b71b6556649
Antony Male <antony.male@gmail.com>
2025-08-27 20:35:06 +0100

    Get a bit of a menu bar going on

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,34 @@
 <script lang="ts">
+    import { resolve } from '$app/paths';
+    import { page } from '$app/state';
     import favicon from '$lib/assets/favicon.svg';
     import '$lib/styles/styles.scss';
     import '@fortawesome/fontawesome-free/css/all.min.css';
 
     let { children } = $props();
+
+    let navItems = [
+        { title: 'Home', url: resolve('/') },
+        { title: 'Settings', url: resolve('/settings') }
+    ];
 </script>
 
 <svelte:head>
     <link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="container-md">
+<nav class="navbar navbar-expand navbar-light bg-light container-fluid">
+    <div class="container-md">
+        <a class="navbar-brand" href={resolve('/')}>Merge Requests</a>
+
+        <div class="navbar-collapse navbar-nav">
+            {#each navItems as nav}
+                <a class="nav-link" class:active={nav.url == page.url.pathname} href={nav.url}>{nav.title}</a>
+            {/each}
+        </div>
+    </div>
+</nav>
+
+<div class="container-md mt-4">
     {@render children?.()}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,8 +13,6 @@
     <title>Merge Requests</title>
 </svelte:head>
 
-<h1>Merge Requests</h1>
-
 {#if !browser || client.state.kind == 'loading'}
     <p>Loading...</p>
 {:else if client.state.kind == 'unconfigured'}
@@ -22,22 +20,23 @@
 {:else if client.state.kind == 'error'}
     <p>Error: {client.state.error}</p>
 {:else}
-    <div class="table-container">
-        <div>
-            <h2>Assigned</h2>
-            <MergeRequestTable mergeRequests={client.assigned ?? []} role="assignee" />
-        </div>
-        <div>
-            <h2>Reviewing</h2>
-            <MergeRequestTable mergeRequests={client.reviewing ?? []} role="reviewer" />
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Assigned</h2>
+                <MergeRequestTable mergeRequests={client.assigned ?? []} role="assignee" />
+            </div>
+            <div class="col-md-6">
+                <h2>Reviewing</h2>
+                <MergeRequestTable mergeRequests={client.reviewing ?? []} role="reviewer" />
+            </div>
         </div>
     </div>
 {/if}
 
-<style>
-    .table-container {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        grid-column-gap: 50px;
+<style lang="scss">
+    h2 {
+        font-size: 1.4rem;
+        margin-bottom: 20px;
     }
 </style>
