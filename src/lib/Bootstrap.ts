e54e4d6282219f4c88342473bef462da0854f0cd
Antony Male <antony.male@gmail.com>
2025-08-30 10:41:45 +0100

    Cache the bootstrap state

--- src/lib/Bootstrap.ts
+++ src/lib/Bootstrap.ts
@@ -2,20 +2,21 @@ import type * as Bootstrap from 'bootstrap';
 import type { Attachment } from 'svelte/attachments';
 import { fromStore, readonly, writable } from 'svelte/store';
 
-const _bootstrap = writable<typeof Bootstrap | null>(null);
-export const bootstrap = readonly(_bootstrap);
+const _bootstrapStore = writable<typeof Bootstrap | null>(null);
+// attachments work better with states, as they re-evaluate properly
+const _bootstrapState = fromStore(_bootstrapStore);
+export const bootstrap = readonly(_bootstrapStore);
 
 export async function loadBootstrap() {
-    _bootstrap.set(await import('bootstrap'));
+    _bootstrapStore.set(await import('bootstrap'));
 }
 
 export function tooltip(options: Partial<Bootstrap.Tooltip.Options>): Attachment {
     return (element) => {
-        const b = fromStore(_bootstrap).current;
-        if (!b) {
+        if (!_bootstrapState.current) {
             return;
         }
-        const tooltip = new b.Tooltip(element, options);
+        const tooltip = new _bootstrapState.current.Tooltip(element, options);
         return () => tooltip.dispose();
     };
 }
