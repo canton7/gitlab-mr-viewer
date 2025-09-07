e51491253ef80caa59edf6447a90ef47fa82b144
Antony Male <antony.male@gmail.com>
2025-09-07 08:54:39 +0100

    Handle turning activity

--- src/lib/gitlab/GitlabClient.svelte.ts
+++ src/lib/gitlab/GitlabClient.svelte.ts
@@ -4,8 +4,12 @@ import { gitlabSettings } from "$lib/Settings";
 import {
     Gitlab as CoreGitlab,
     type CommitablePipelineStatus,
+    type DiscussionNoteSchema,
+    type DiscussionSchema,
     type ExpandedUserSchema,
+    type MergeRequestDiscussionNoteSchema,
     type MergeRequestSchemaWithBasicLabels,
+    type UserSchema,
 } from "@gitbeaker/core";
 import { createRequesterFn, type RequestOptions, type ResourceOptions } from "@gitbeaker/requester-utils";
 import { createRequestHandler } from "./GitlabUtils";
@@ -13,11 +17,16 @@ import { GitlabCache } from "./GitlabCache";
 
 const UPDATE_PERIOD_MS = 1 * 60 * 1000;
 const CACHE_FLUSH_PERIOD_MS = 15 * 60 * 1000;
+// Activities which occurr within 10 mins of each other get combined
+const COMBINE_ACTIVITY_TIME_MS = 10 * 60 * 1000;
 
 export type GitlabCiStatus = CommitablePipelineStatus | "none";
 
+export type MergeRequestType = "assignee" | "reviewer";
+
 export interface MergeRequest {
     key: string;
+    type: MergeRequestType;
     title: string;
     webUrl: string;
     createdAt: Date;
@@ -37,7 +46,7 @@ export interface Activity {
     key: string;
     body: string;
     updatedAt: Date;
-    noteId: number;
+    noteId: number | null;
     authorName: string;
     mergeRequest: MergeRequest;
 }
@@ -88,7 +97,8 @@ export class GitlabClient {
 
     private async mapMergeRequest(
         api: CoreGitlab,
-        mergeRequest: MergeRequestSchemaWithBasicLabels
+        mergeRequest: MergeRequestSchemaWithBasicLabels,
+        type: MergeRequestType
     ): Promise<[MergeRequest, Activity[]]> {
         const [approvals, discussions, commitStatus] = await Promise.all([
             await api.MergeRequestApprovals.showConfiguration(mergeRequest.project_id, {
@@ -123,6 +133,7 @@ export class GitlabClient {
 
         const mr: MergeRequest = {
             key: `${mergeRequest.project_id}-${mergeRequest.id}`,
+            type: type,
             title: mergeRequest.title,
             webUrl: mergeRequest.web_url,
             createdAt: new Date(mergeRequest.created_at),
@@ -138,34 +149,131 @@ export class GitlabClient {
             ciLink: commitStatus.at(0)?.target_url ?? null,
         };
 
-        let activities: Activity[] = [];
-        for (const discussion of discussions) {
-            if (discussion.notes != undefined && discussion.notes.length > 0) {
-                let body;
-                if (discussion.individual_note) {
-                    body = discussion.notes[0].body;
-                } else {
-                    body = `left ${discussion.notes.length} comment(s)`;
-                }
-                activities.push({
-                    key: discussion.id,
-                    body: body,
-                    updatedAt: new Date(discussion.notes[0].updated_at),
-                    noteId: discussion.notes[0].id,
-                    authorName: discussion.notes[0].author.name,
-                    mergeRequest: mr,
-                });
-            }
-        }
+        const activities = this.createActivities(type, discussions, mr);
 
         return [mr, activities];
     }
 
+    private createActivities(type: MergeRequestType, discussions: DiscussionSchema[], mergeRequest: MergeRequest) {
+        // Discussions are presented in as a list of threads, but each thread may have multiple comments left at
+        // different times.
+
+        let activities: Activity[] = [];
+
+        let commentNotes: DiscussionNoteSchema[] = [];
+        // let resolvedNotes: MergeRequestDiscussionNoteSchema[] = [];
+
+        for (const discussion of discussions) {
+            if (discussion.notes === undefined || discussion.notes.length == 0) {
+                continue;
+            }
+
+            // These are things like system notifications, X added commits, etc
+            if (discussion.individual_note) {
+                let body: string | null = discussion.notes[0].body.split("\n", 2)[0];
+                // We don't want to show this one
+                if (body == "left review comments") {
+                    body = null;
+                }
+
+                if (body) {
+                    // It's technically possible for the same MR to show up in "reviewing" and "assigned"
+                    activities.push({
+                        key: `${discussion.id}-${type}`,
+                        body: body,
+                        updatedAt: new Date(discussion.notes[0].updated_at),
+                        noteId: discussion.notes[0].id,
+                        authorName: discussion.notes[0].author.name,
+                        mergeRequest: mergeRequest,
+                    });
+                }
+            } else {
+                // We'll synthesise events for:
+                // - X left N comments
+                // - X closed N threads
+                commentNotes.push(...discussion.notes);
+
+                // If a thread is resolved, all times in it get a resolved status
+                // if (discussion.notes[0].resolved_at != undefined) {
+                //     resolvedNotes.push(discussion.notes[0] as MergeRequestDiscussionNoteSchema);
+                // }
+            }
+        }
+
+        function collectSimilar(
+            synthesisedNoteType: string,
+            notes: DiscussionNoteSchema[],
+            dateGetter: (current: DiscussionNoteSchema) => Date,
+            authorGetter: (current: DiscussionNoteSchema) => { id: number; name: string },
+            messageGetter: (count: number) => string
+        ) {
+            type Collection = { end: Date; firstNote: DiscussionNoteSchema; count: number };
+
+            const appendComments = (current: Collection) =>
+                activities.push({
+                    key: `${synthesisedNoteType}-${current.firstNote.id}-${type}`,
+                    body: messageGetter(current.count),
+                    updatedAt: current.end,
+                    noteId: null,
+                    authorName: authorGetter(current.firstNote).name,
+                    mergeRequest: mergeRequest,
+                });
+
+            let current: Collection | null = null;
+            for (const note of notes) {
+                const date = dateGetter(note);
+                if (
+                    current != null &&
+                    authorGetter(current.firstNote).id == authorGetter(current.firstNote).id &&
+                    date.getTime() - current.end.getTime() < COMBINE_ACTIVITY_TIME_MS
+                ) {
+                    current.end = date;
+                    current.count++;
+                } else {
+                    if (current != null) {
+                        appendComments(current);
+                    }
+                    current = { end: date, firstNote: note, count: 1 };
+                }
+            }
+            if (current != null) {
+                appendComments(current);
+            }
+        }
+
+        // First, adding comments
+        commentNotes.sort((x, y) => new Date(x.updated_at).getTime() - new Date(y.updated_at).getTime());
+
+        collectSimilar(
+            "synthesised-add-comments",
+            commentNotes,
+            // The updated time gets updated when we resolve a comment, so we need to use the created time
+            (note) => new Date(note.created_at),
+            (note) => note.author,
+            (count) => `added ${count} comment(s)`
+        );
+
+        // Then, resolving
+        // resolvedNotes.sort((x, y) => new Date(x.resolved_at).getTime() - new Date(y.resolved_at).getTime());
+
+        // collectSimilar(
+        //     "synthesised-resolve-comments",
+        //     resolvedNotes,
+        //     (note) => new Date(note.resolved_at),
+        //     // @ts-ignore - the type annotation is wrong, and this does contain the correct type
+        //     (note) => note.resolved_by,
+        //     (count) => `resolved ${count} thread(s)`
+        // );
+
+        return activities;
+    }
+
     private async mapMergeRequests(
         api: CoreGitlab,
-        merge_requests: Promise<MergeRequestSchemaWithBasicLabels[]>
+        merge_requests: Promise<MergeRequestSchemaWithBasicLabels[]>,
+        type: MergeRequestType
     ): Promise<[MergeRequest[], Activity[]]> {
-        const results = await Promise.all((await merge_requests).map((x) => this.mapMergeRequest(api, x)));
+        const results = await Promise.all((await merge_requests).map((x) => this.mapMergeRequest(api, x, type)));
         return [results.map((x) => x[0]), results.flatMap((x) => x[1])];
     }
 
@@ -192,7 +300,8 @@ export class GitlabClient {
                         scope: "all",
                         reviewerId: userId,
                         orderBy: "updated_at",
-                    })
+                    }),
+                    "reviewer"
                 ),
                 this.mapMergeRequests(
                     api,
@@ -201,22 +310,16 @@ export class GitlabClient {
                         scope: "all",
                         assigneeId: userId,
                         orderBy: "updated_at",
-                    })
+                    }),
+                    "assignee"
                 ),
             ]);
 
             this.reviewing = reviewing;
             this.assigned = assigned;
 
-            // We might have duplicates (if an MR appears in both reviewing and assigned). That's OK.
-            let activitiesLookup = new Map<string, Activity>();
-            for (const activity of reviewingActivity.concat(assignedActivity)) {
-                activitiesLookup.set(activity.key, activity);
-            }
-            // TODO: Sort
-            this.activities = activitiesLookup
-                .values()
-                .toArray()
+            this.activities = reviewingActivity
+                .concat(assignedActivity)
                 .sort((x, y) => y.updatedAt.getTime() - x.updatedAt.getTime());
         });
     }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,25 +36,44 @@
 {:else if client.state.kind == "error"}
     <p>Error: {client.state.error}</p>
 {:else}
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <h2>Assigned</h2>
-                <MergeRequestTable mergeRequests={client.assigned ?? []} role="assignee" />
-            </div>
-            <div class="col-md-6">
-                <h2>Reviewing</h2>
-                <MergeRequestTable mergeRequests={client.reviewing ?? []} role="reviewer" />
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Assigned</h2>
+                    <MergeRequestTable mergeRequests={client.assigned ?? []} role="assignee" />
+                </div>
+                <div class="col-md-6">
+                    <h2>Reviewing</h2>
+                    <MergeRequestTable mergeRequests={client.reviewing ?? []} role="reviewer" />
+                </div>
             </div>
         </div>
-    </div>
 
-    <ActivityTable activities={client.activities} />
+        <div class="activities">
+            <ActivityTable activities={client.activities} />
+        </div>
+    </div>
 {/if}
 
 <style lang="scss">
     h2 {
         font-size: 1.4rem;
         margin-bottom: 20px;
+    }
+
+    .content {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+
+    .container {
+        flex: 2;
+    }
+
+    .activities {
+        flex: 1;
+        overflow-y: scroll;
     }
 </style>
