f7b6b1f5ea38681d1e5bcd8923e40bc8443074fa
Antony Male <antony.male@gmail.com>
2025-09-15 09:20:01 +0100

    Show a link to generate a PAT

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -5,6 +5,8 @@
     import { goto } from "$app/navigation";
     import { resolve } from "$app/paths";
 
+    let baseUrl = $state($gitlabSettings.baseUrl);
+
     const save: SubmitFunction = ({ formData, cancel }) => {
         cancel();
         $gitlabSettings.baseUrl = formData.get("baseUrl") as string;
@@ -24,7 +26,12 @@
         <label for="baseUrl" class="form-label">GitLab URL</label>
         <div class="input-group">
             <span class="input-group-text">https://</span>
-            <input name="baseUrl" class="form-control" required value={$gitlabSettings.baseUrl} />
+            <input
+                name="baseUrl"
+                class="form-control"
+                required
+                value={$gitlabSettings.baseUrl}
+                onblur={(e) => (baseUrl = (e.target as HTMLInputElement).value)} />
         </div>
         <div class="form-text">Don't include 'rest/api' etc</div>
     </div>
@@ -32,7 +39,13 @@
     <div class="mb-3">
         <label for="accessToken" class="form-label">GitLab Personal Access Token</label>
         <input type="password" class="form-control" name="accessToken" required value={$gitlabSettings.accessToken} />
-        <div class="form-text">This needs to have the read_api scope</div>
+        <div class="form-text">
+            {#if baseUrl}
+                {@const url = `https://${baseUrl}/-/user_settings/personal_access_tokens`}
+                Generate at <a href={url} target="_blank">{url}</a>.
+            {/if}
+            This needs to have the read_api scope
+        </div>
     </div>
 
     <input type="submit" class="btn btn-primary" value="Save" />
