5134285cc1e46ca2ef98df89376aa2399221afe6
Antony Male <antony.male@gmail.com>
2025-09-21 10:01:01 +0100

    Fix times showing as e.g. 'in 5 seconds'

--- src/lib/DateUtils.ts
+++ src/lib/DateUtils.ts
@@ -1,4 +1,5 @@
-import { readable } from "svelte/store";
+import moment from "moment";
+import { derived, readable } from "svelte/store";
 
 export const DATE_FORMAT = "ddd DD MMMM YYYY [at] h:mm:ss a";
 
@@ -12,4 +13,14 @@ export const now = readable(new Date(), (set) => {
     }, NOW_UPDATE_INTERVAL_MS);
 
     return () => clearInterval(interval);
+});
+
+/**
+ * $now can lag, and if it's a bit behind, then doing 'moment(someDate).from($now)' can show e.g. 'in 5 seconds'.
+ *
+ * Instead, use '$fromNow(someDate)'.
+ */
+export const fromNow = derived(now, ($now) => (date: Date | string) => {
+    const nowMoment = moment(date);
+    return nowMoment.from(moment.max(nowMoment, moment($now)));
 });
